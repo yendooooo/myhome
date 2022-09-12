59b4743e5f10b8862f9a94607d348e09533d93e7
yendoo <codetree2@gmail.com>
2022-09-12 15:14:03 +0900

    init

---
+++ src/test/java/com/yendoo/my/home/MyHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yendoo.my.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
